72cabe1df2ec3e9cd31dc6a73e539d7086eed6f7
gilacost <gilacost@users.noreply.github.com>
2019-01-24 12:13:24 +0100

    ya

--- zsh/zsh.mk
+++ zsh/zsh.mk
@@ -4,10 +4,16 @@ ifdef ZSH
 INSTALLERS += zsh
 CLEANERS   += clean_zsh
 
+ZPREZTORC_SRC     := $(DOTFILES)/zsh/zpreztorc
+ZPREZTORC         := $(DST_DIR)/.zpreztorc
 ZSHRC_SRC         := $(DOTFILES)/zsh/zshrc
 ZSHRC             := $(DST_DIR)/.zshrc
 ZPROFILE_SRC      := $(DOTFILES)/zsh/zprofile
 ZPROFILE          := $(DST_DIR)/.zprofile
+
+# git clone https://github.com/bhilburn/powerlevel9k.git  ~/.zprezto/modules/prompt/external/powerlevel9k && \
+# ln -s ~/.zprezto/modules/prompt/external/powerlevel9k/powerlevel9k.zsh-theme ~/.zprezto/modules/prompt/functions/promp
+# t_powerlevel9k_setup
 OH_MY_ZSH_URL     := git://github.com/robbyrussell/oh-my-zsh.git
 OH_MY_ZSH_DST     := $(DST_DIR)/.oh-my-zsh
 BASE16_COLORS_DST := $(CONFIG_DIR)/base16-shell
@@ -15,7 +21,10 @@ BASE16_COLORS_URL := https://github.com/chriskempson/base16-shell.git
 
 .PHONY: zsh clean_zsh
 
-zsh: banner_install_zsh $(ZSHRC) $(ZPROFILE) $(OH_MY_ZSH_DST) $(BASE16_COLORS_DST)
+zsh: banner_install_zsh $(ZPREZTORC) $(ZSHRC) $(ZPROFILE) $(OH_MY_ZSH_DST) $(BASE16_COLORS_DST)
+
+$(ZPREZTORC):
+	$(LINK) $(ZPREZTORC_SRC) $@
 
 $(ZSHRC):
 	$(LINK) $(ZSHRC_SRC) $@
