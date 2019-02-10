5e19c5aa781fdba09daf3b45fa4776c7cf037523
gilacost <gilacost@users.noreply.github.com>
2019-02-10 23:31:58 +0100

    chore: this is beautyfull

--- zsh/zsh.mk
+++ zsh/zsh.mk
@@ -3,17 +3,17 @@ ZSH := $(shell command -v zsh 2>/dev/null)
 ifdef ZSH
 INSTALLERS += zsh
 CLEANERS   += clean_zsh
+# git clone https://github.com/eendroroy/alien.git
+# cd alien
+# git submodule update --init --recursive
 
-ZPREZTORC_SRC     := $(DOTFILES)/zsh/zpreztorc
-ZPREZTORC         := $(DST_DIR)/.zpreztorc
+git clone https://github.com/eendroroy/alien.git && cd alien && git submodule update --init --recursive
+
 ZSHRC_SRC         := $(DOTFILES)/zsh/zshrc
 ZSHRC             := $(DST_DIR)/.zshrc
 ZPROFILE_SRC      := $(DOTFILES)/zsh/zprofile
 ZPROFILE          := $(DST_DIR)/.zprofile
 
-# git clone https://github.com/bhilburn/powerlevel9k.git  ~/.zprezto/modules/prompt/external/powerlevel9k && \
-# ln -s ~/.zprezto/modules/prompt/external/powerlevel9k/powerlevel9k.zsh-theme ~/.zprezto/modules/prompt/functions/promp
-# t_powerlevel9k_setup
 OH_MY_ZSH_URL     := git://github.com/robbyrussell/oh-my-zsh.git
 OH_MY_ZSH_DST     := $(DST_DIR)/.oh-my-zsh
 BASE16_COLORS_DST := $(CONFIG_DIR)/base16-shell
@@ -21,10 +21,7 @@ BASE16_COLORS_URL := https://github.com/chriskempson/base16-shell.git
 
 .PHONY: zsh clean_zsh
 
-zsh: banner_install_zsh $(ZPREZTORC) $(ZSHRC) $(ZPROFILE) $(OH_MY_ZSH_DST) $(BASE16_COLORS_DST)
-
-$(ZPREZTORC):
-	$(LINK) $(ZPREZTORC_SRC) $@
+zsh: banner_install_zsh $(ZSHRC) $(ZPROFILE) $(OH_MY_ZSH_DST) $(BASE16_COLORS_DST)
 
 $(ZSHRC):
 	$(LINK) $(ZSHRC_SRC) $@
