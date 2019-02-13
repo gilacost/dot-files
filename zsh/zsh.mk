e155a705cadd3ce07c715f2d63e4ead6dc8bf1de
gilacost <gilacost@users.noreply.github.com>
2019-02-13 22:56:48 +0100

    chore: this is beautyfull

--- zsh/zsh.mk
+++ zsh/zsh.mk
@@ -5,6 +5,8 @@ INSTALLERS += zsh
 CLEANERS   += clean_zsh
 # git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 # git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+# git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+# ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
 
 ZSHRC_SRC         := $(DOTFILES)/zsh/zshrc
