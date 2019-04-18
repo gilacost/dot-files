5cb0936d1d67d1645e2c93e111cd2dae7f050b4f
gilacost <josep.giralt@derivco.se>
2019-04-18 18:18:59 +0200

    closer

--- zsh/zsh.mk
+++ zsh/zsh.mk
@@ -3,26 +3,31 @@ ZSH := $(shell command -v zsh 2>/dev/null)
 ifdef ZSH
 INSTALLERS += zsh
 CLEANERS   += clean_zsh
-# git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+
+ZSHRC_SRC								:= $(DOTFILES)/zsh/zshrc
+ZSHRC										:= $(DST_DIR)/.zshrc
+
+OH_MY_ZSH_URL						:= git://github.com/robbyrussell/oh-my-zsh.git
+OH_MY_ZSH_DST						:= $(DST_DIR)/.oh-my-zsh
+
+ZSH_AUTOSUGGESTIONS_URL := git://github.com/zsh-users/zsh-autosuggestions.git
+ZSH_AUTOSUGGESTIONS_DST := @$ZSH_CUSTOM/plugins/zsh-autosuggestion
 # git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 # git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 # ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-
-
-ZSHRC_SRC         := $(DOTFILES)/zsh/zshrc
-ZSHRC             := $(DST_DIR)/.zshrc
-
-OH_MY_ZSH_URL     := git://github.com/robbyrussell/oh-my-zsh.git
-OH_MY_ZSH_DST     := $(DST_DIR)/.oh-my-zsh
-BASE16_COLORS_DST := $(CONFIG_DIR)/base16-shell
-BASE16_COLORS_URL := https://github.com/chriskempson/base16-shell.git
+BASE16_COLORS_DST				:= $(CONFIG_DIR)/base16-shell
+BASE16_COLORS_URL				:= https://github.com/chriskempson/base16-shell.git
 
 .PHONY: zsh clean_zsh
 
-zsh: banner_install_zsh $(ZSHRC) $(OH_MY_ZSH_DST) $(BASE16_COLORS_DST)
+zsh: banner_install_zsh $(ZSHRC) $(OH_MY_ZSH_DST) $(ZSH_AUTOSUGGESTIONS_DST) $(BASE16_COLORS_DST)
 
 $(ZSHRC):
 	$(LINK) $(ZSHRC_SRC) $@
+
+$(ZSH_AUTOSUGGESTIONS_DST):
+	$(CLONE) $(ZSH_AUTOSUGGESTIONS_URL) $@
 
 $(BASE16_COLORS_DST):
 	$(CLONE) $(BASE16_COLORS_URL) $@
