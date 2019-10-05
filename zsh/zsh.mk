13db71406021ee029f0da76d3fcbe37dc29ec815
gilacost <josep.giralt@derivco.se>
2019-10-05 17:43:14 +0200

    make file link for 10k

--- zsh/zsh.mk
+++ zsh/zsh.mk
@@ -8,30 +8,28 @@ CLEANERS   += clean_zsh
 ZSHRC_SRC								:= $(DOTFILES)/zsh/zshrc
 ZSHRC										:= $(DST_DIR)/.zshrc
 
+P10K_SRC								:= $(DOTFILES)/zsh/p10k.zsh
+P10K    								:= $(DST_DIR)/.p10k.zsh
+
 OH_MY_ZSH_URL						:= git://github.com/robbyrussell/oh-my-zsh.git
 OH_MY_ZSH_DST						:= $(DST_DIR)/.oh-my-zsh
 
-# ZSH_AUTOSUGGESTIONS_URL := git://github.com/zsh-users/zsh-autosuggestions.git
-# ZSH_AUTOSUGGESTIONS_DST := $ZSH_CUSTOM/plugins/zsh-autosuggestion
 .PHONY: zsh clean_zsh
 
-# zsh: banner_install_zsh $(ZSHRC) $(OH_MY_ZSH_DST) $(ZSH_AUTOSUGGESTIONS_DST) $(BASE16_COLORS_DST)
-zsh: banner_install_zsh $(ZSHRC) $(OH_MY_ZSH_DST)
+zsh: banner_install_zsh $(ZSHRC) $(OH_MY_ZSH_DST) $(P10K)
 
 $(ZSHRC):
 	$(LINK) $(ZSHRC_SRC) $@
 
-# $(ZSH_AUTOSUGGESTIONS_DST):
-# 	$(CLONE) $(ZSH_AUTOSUGGESTIONS_URL) $@
+$(P10K):
+	$(LINK) $(P10K_SRC) $@
 
 $(OH_MY_ZSH_DST):
 	$(CLONE) $(OH_MY_ZSH_URL) $@
 
 clean_zsh: banner_clean_zsh
 	$(RM) $(ZSHRC)
-	$(RM) $(ZPROFILE)
 	$(RM) $(OH_MY_ZSH_DST)
+	$(RM) $(P10K)
 
 endif
-
-
