200afdc763874dfc751f8d358c36990321ca671c
Josep Giralt D'Lacoste <josepgiraltdlacoste@gmail.com>
2021-09-15 11:03:50 +0100

    Add pending system defaults

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -2,13 +2,42 @@
 
 {
 
-  nix.package = pkgs.nixFlakes;
+  # environment.shells = [ pkgs.zsh ];
+  environment.systemPackages = [ pkgs.zsh pkgs.gcc ];
+  users = {
 
-  nix.extraOptions = ''
-    experimental-features = nix-command flakes
-    keep-outputs = true
-    keep-derivations = true
-  '';
+    nix.configureBuildUsers = true;
+    users = {
+      pepo = {
+        home = "/Users/pepo";
+        shell = pkgs.zsh;
+      };
+    };
+
+  };
+  programs.bash.enable = false;
+  programs.zsh.enable = true;
+
+  nix = {
+    trustedUsers = [ "root" "pepo" ];
+    package = pkgs.nixFlakes;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+      keep-outputs = true
+      keep-derivations = true
+    '';
+    gc = {
+      automatic = true;
+      options = "--delete-older-than 14d";
+    };
+    # binaryCachePublicKeys =
+    #   [ "cache.daiderd.com-1:R8KOWZ8lDaLojqD+v9dzXAqGn29gEzPTTbr/GIpCTrI=" ];
+    # trustedBinaryCaches = [ "https://d3i7ezr9vxxsfy.cloudfront.net" ];
+    # # review sandboxPaths
+    # sandboxPaths = [ "/private/tmp" "/private/var/tmp" "/usr/bin/env" ];
+  };
+
+  nixpkgs.config.allowUnfree = true;
 
   ############
   #  System  #
@@ -16,13 +45,151 @@
 
   time.timeZone = "Europe/London";
 
-  #####TODO##########
+  system.activationScripts.postActivation.text = ''
+    # TODO: Rewrite as much of these as possible using nix-darwin
 
-  ##bluetooth in menu bar
-  #open '/System/Library/CoreServices/Menu Extras/Bluetooth.menu'
-  # system.defaults.".GlobalPreferences"."com.apple.screensaver askForPasswordDelay"=1;
-  # system.defaults.".GlobalPreferences"."com.apple.screensaver askForPassword"=1;
-  # system.defaults.".GlobalPreferences"."com.apple.menuextra.battery"="YES";
+    # From https://github.com/mathiasbynens/dotfiles/blob/master/.macos taken
+    # at commit e72d1060f3df8c157f93af52ea59508dae36ef50.
+
+    ###############################################################################
+    # General UI/UX                                                               #
+    ###############################################################################
+
+    # Disable the sound effects on boot
+    sudo nvram SystemAudioVolume=" "
+
+    # Set highlight color to ORANGE
+    defaults write NSGlobalDomain AppleHighlightColor -string "0.764700 0.976500 0.568600"
+
+    # Automatically quit printer app once the print jobs complete
+    defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+    # Reveal IP address, hostname, OS version, etc. when clicking the clock
+    # in the login window
+    sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
+
+    # Restart automatically if the computer freezes
+    sudo systemsetup -setrestartfreeze on
+
+    # Never go into computer sleep mode
+    sudo systemsetup -setcomputersleep Off > /dev/null
+
+    # Disable Notification Center and remove the menu bar icon
+    launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
+
+    ###############################################################################
+    # SSD-specific tweaks                                                         #
+    ###############################################################################
+
+    # Disable hibernation (speeds up entering sleep mode)
+    sudo pmset -a hibernatemode 0
+
+    ###############################################################################
+    # Trackpad, mouse, keyboard, Bluetooth accessories, and input                 #
+    ###############################################################################
+
+    # Shows battery percentage
+    defaults write com.apple.menuextra.battery ShowPercent YES
+
+    # Increase sound quality for Bluetooth headphones/headsets
+    defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
+
+    # Follow the keyboard focus while zoomed in
+    defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
+
+    # Show language menu in the top right corner of the boot screen
+    sudo defaults write /Library/Preferences/com.apple.loginwindow showInputMenu -bool true
+
+    # Set language and text formats
+    # Note: if you’re in the US, replace `EUR` with `USD`, `Centimeters` with
+    # `Inches`, `en_GB` with `en_US`, and `true` with `false`.
+    defaults write NSGlobalDomain AppleLanguages -array "en"
+    defaults write NSGlobalDomain AppleLocale -string "en_GB@currency=GBP"
+    defaults write NSGlobalDomain AppleMeasurementUnits -string "Inches"
+    defaults write NSGlobalDomain AppleMetricUnits -bool true
+
+    # Stop iTunes from responding to the keyboard media keys
+    launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2>/dev/null
+
+    ###############################################################################
+    # Screen                                                                      #
+    ###############################################################################
+
+    # Require password immediately after sleep or screen saver begins
+    defaults write com.apple.screensaver askForPassword -int 1
+    defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+    # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+    defaults write com.apple.screencapture type -string "png"
+
+    # Disable shadow in screenshots
+    defaults write com.apple.screencapture disable-shadow -bool true
+
+    # Enable HiDPI display modes (requires restart)
+    sudo defaults write /Library/Preferences/com.apple.windowserver DisplayResolutionEnabled -bool true
+
+    ###############################################################################
+    # Finder                                                                      #
+    ###############################################################################
+
+    # Finder: disable window animations and Get Info animations
+    defaults write com.apple.finder DisableAllAnimations -bool true
+
+    # Set Desktop as the default location for new Finder windows
+    # For other paths, use `PfLo` and `file:///full/path/here/`
+    defaults write com.apple.finder NewWindowTarget -string "PfDe"
+    defaults write com.apple.finder NewWindowTargetPath -string "file://$HOME/Desktop/"
+
+    # Show icons for hard drives, servers, and removable media on the desktop
+    defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+    defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+    defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
+    defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+
+    # Finder: show status bar
+    defaults write com.apple.finder ShowStatusBar -bool true
+
+    # Finder: show path bar
+    defaults write com.apple.finder ShowPathbar -bool true
+
+    # Keep folders on top when sorting by name
+    defaults write com.apple.finder _FXSortFoldersFirst -bool true
+
+    # When performing a search, search the current folder by default
+    defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+    # Avoid creating .DS_Store files on network or USB volumes
+    defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+    defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+    # Automatically open a new Finder window when a volume is mounted
+    defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+    defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+    defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
+
+    # Use list view in all Finder windows by default
+    # Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
+    defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+
+    # Disable the warning before emptying the Trash
+    defaults write com.apple.finder WarnOnEmptyTrash -bool false
+
+    # Enable AirDrop over Ethernet and on unsupported Macs running Lion
+    defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
+
+    # Show the ~/Library folder
+    chflags nohidden ~/Library
+
+    # Show the /Volumes folder
+    sudo chflags nohidden /Volumes
+
+    # Expand the following File Info panes:
+    # “General”, “Open with”, and “Sharing & Permissions”
+    defaults write com.apple.finder FXInfoPanesExpanded -dict \
+      General -bool true \
+      OpenWith -bool true \
+      Privileges -bool true
+  '';
 
   system = {
     defaults = {
@@ -158,17 +325,11 @@
     "slack"
   ];
 
-  nix.binaryCachePublicKeys =
-    [ "cache.daiderd.com-1:R8KOWZ8lDaLojqD+v9dzXAqGn29gEzPTTbr/GIpCTrI=" ];
-  nix.trustedBinaryCaches = [ "https://d3i7ezr9vxxsfy.cloudfront.net" ];
-  # review sandboxPaths
-  nix.sandboxPaths = [ "/private/tmp" "/private/var/tmp" "/usr/bin/env" ];
   programs.nix-index.enable = true;
 
   environment.variables.LANG = "en_GB.UTF-8";
   environment.loginShell = "${pkgs.zsh}/bin/zsh -l";
 
-  nixpkgs.config.allowUnfree = true;
   # Recreate /run/current-system symlink after boot
   services.activate-system.enable = true;
 }
