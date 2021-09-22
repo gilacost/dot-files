91fa3f462f71faab09eff952a73ba0ba80d1b961
Josep Giralt D'Lacoste <josepgiraltdlacoste@gmail.com>
2021-09-22 21:22:12 +0100

    Enable unfree packages for one password plugin

---
+++ config.nix
@@ -0,0 +1,8 @@
+{
+  packageOverrides = pkgs: {
+    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+      inherit pkgs;
+    };
+  };
+  allowUnfree = true;
+}
