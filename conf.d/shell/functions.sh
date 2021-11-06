64b76e16b1e535283ddac22186441c9af2b5f552
Josep Lluis Giralt D'Lacoste <pep.g.dlacoste@erlang-solutions.com>
2021-11-06 09:21:10 +0000

    Flakify nixify functions

--- conf.d/shell/functions.sh
+++ conf.d/shell/functions.sh
@@ -58,8 +58,37 @@ function erlv {
   erl -eval 'erlang:display(erlang:system_info(otp_release)), halt().'  -noshell
 }
 
+nixify() {
+  if [ ! -e ./.envrc ]; then
+    echo "use nix" > .envrc
+    direnv allow
+  fi
+  if [[ ! -e shell.nix ]] && [[ ! -e default.nix ]]; then
+    cat > default.nix <<'EOF'
+with import <nixpkgs> {};
+mkShell {
+  nativeBuildInputs = [
+    bashInteractive
+  ];
+}
+EOF
+    ${EDITOR:-vim} default.nix
+  fi
+}
+
+flakifiy() {
+  if [ ! -e flake.nix ]; then
+    nix flake new -t github:nix-community/nix-direnv .
+  elif [ ! -e .envrc ]; then
+    echo "use flake" > .envrc
+    direnv allow
+  fi
+  ${EDITOR:-vim} flake.nix
+}
+
 source <(kubectl completion zsh)
 alias k=kubectl
 complete -F __start_kubectl k
 
 source $HOME/.zshrc_local
+
