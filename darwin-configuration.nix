60b692748c1f67bca8b0c15bc6082c50f5c13ba0
Josep Lluis Giralt D'Lacoste <josepgiraltdlacoste@gmail.com>
2022-08-09 10:18:29 +0100

    no amphetamine

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -38,158 +38,6 @@
 
   time.timeZone = "Europe/London";
 
-  system.activationScripts.postActivation.text = ''
-    # TODO: Rewrite as much of these as possible using nix-darwin
-
-    # From https://github.com/mathiasbynens/dotfiles/blob/master/.macos taken
-    # at commit e72d1060f3df8c157f93af52ea59508dae36ef50.
-
-    ###############################################################################
-    # General UI/UX                                                               #
-    ###############################################################################
-
-    # Disable the sound effects on boot
-    sudo nvram SystemAudioVolume=" "
-
-    # Disable UI alert audio
-    defaults write com.apple.systemsound "com.apple.sound.uiaudio.enabled" -int 0
-    # todo FLASH
-    # plutil -convert xml1 -o - ~/Library/Preferences/com.apple.universalaccess.plist|grep -A1 flashScreen
-    # <key>flashScreen</key> <false/>
-
-    # Set highlight color to ORANGE
-    defaults write NSGlobalDomain AppleHighlightColor -string "0.764700 0.976500 0.568600"
-
-    # Automatically quit printer app once the print jobs complete
-    defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
-
-    # Reveal IP address, hostname, OS version, etc. when clicking the clock
-    # in the login window
-    sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
-
-    # Restart automatically if the computer freezes
-    sudo systemsetup -setrestartfreeze on
-
-    # Never go into computer sleep mode
-    sudo systemsetup -setcomputersleep Off > /dev/null
-
-    # Disable Notification Center and remove the menu bar icon
-    launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
-
-    ###############################################################################
-    # SSD-specific tweaks                                                         #
-    ###############################################################################
-
-    # Disable hibernation (speeds up entering sleep mode)
-    sudo pmset -a hibernatemode 0
-
-    ###############################################################################
-    # Trackpad, mouse, keyboard, Bluetooth accessories, and input                 #
-    ###############################################################################
-
-    # Shows battery percentage
-    defaults write com.apple.menuextra.battery ShowPercent YES; killall SystemUIServer
-
-    # Increase sound quality for Bluetooth headphones/headsets
-    defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
-
-    # Follow the keyboard focus while zoomed in
-    defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
-
-    # Show language menu in the top right corner of the boot screen
-    sudo defaults write /Library/Preferences/com.apple.loginwindow showInputMenu -bool true
-
-    # Set language and text formats
-    # Note: if you’re in the US, replace `EUR` with `USD`, `Centimeters` with
-    # `Inches`, `en_GB` with `en_US`, and `true` with `false`.
-    defaults write NSGlobalDomain AppleLanguages -array "en"
-    defaults write NSGlobalDomain AppleLocale -string "en_GB@currency=GBP"
-    defaults write NSGlobalDomain AppleMeasurementUnits -string "Inches"
-    defaults write NSGlobalDomain AppleMetricUnits -bool true
-
-    # Stop iTunes from responding to the keyboard media keys
-    launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2>/dev/null
-
-    ###############################################################################
-    # Screen                                                                      #
-    ###############################################################################
-
-    # Require password immediately after sleep or screen saver begins
-    defaults write com.apple.screensaver askForPassword -int 1
-    defaults write com.apple.screensaver askForPasswordDelay -int 0
-
-    # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
-    defaults write com.apple.screencapture type -string "png"
-
-    # Disable shadow in screenshots
-    defaults write com.apple.screencapture disable-shadow -bool true
-
-    # Enable HiDPI display modes (requires restart)
-    sudo defaults write /Library/Preferences/com.apple.windowserver DisplayResolutionEnabled -bool true
-
-    ###############################################################################
-    # Finder                                                                      #
-    ###############################################################################
-
-    # Finder: disable window animations and Get Info animations
-    defaults write com.apple.finder DisableAllAnimations -bool true
-
-    # Set Desktop as the default location for new Finder windows
-    # For other paths, use `PfLo` and `file:///full/path/here/`
-    defaults write com.apple.finder NewWindowTarget -string "PfDe"
-    defaults write com.apple.finder NewWindowTargetPath -string "file://$HOME/Desktop/"
-
-    # Show icons for hard drives, servers, and removable media on the desktop
-    defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
-    defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
-    defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
-    defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
-
-    # Finder: show status bar
-    defaults write com.apple.finder ShowStatusBar -bool true
-
-    # Finder: show path bar
-    defaults write com.apple.finder ShowPathbar -bool true
-
-    # Keep folders on top when sorting by name
-    defaults write com.apple.finder _FXSortFoldersFirst -bool true
-
-    # When performing a search, search the current folder by default
-    defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
-
-    # Avoid creating .DS_Store files on network or USB volumes
-    defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-    defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
-
-    # Automatically open a new Finder window when a volume is mounted
-    defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
-    defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
-    defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
-
-    # Use list view in all Finder windows by default
-    # Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
-    defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
-
-    # Disable the warning before emptying the Trash
-    defaults write com.apple.finder WarnOnEmptyTrash -bool false
-
-    # Enable AirDrop over Ethernet and on unsupported Macs running Lion
-    defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
-
-    # Show the ~/Library folder
-    chflags nohidden ~/Library
-
-    # Show the /Volumes folder
-    sudo chflags nohidden /Volumes
-
-    # Expand the following File Info panes:
-    # “General”, “Open with”, and “Sharing & Permissions”
-    defaults write com.apple.finder FXInfoPanesExpanded -dict \
-      General -bool true \
-      OpenWith -bool true \
-      Privileges -bool true
-  '';
-
   system = {
     defaults = {
       NSGlobalDomain = {
@@ -272,42 +120,74 @@
 
   services.yabai = {
     enable = true;
-    package = pkgs.yabai;
     enableScriptingAddition = true;
+    package = pkgs.yabai;
     config = {
-      window_border = "on";
-      window_border_width = 5;
-      active_window_border_color = "0xffd9adad";
-      normal_window_border_color = "0xff3b4252";
-      focus_follows_mouse = "off";
-      mouse_follows_focus = "off";
-      mouse_drop_action = "stack";
+      # layout
+      layout = "bsp";
+      auto_balance = "on";
+      split_ratio = "0.50";
       window_placement = "second_child";
+      # Gaps
+      window_gap = 18;
+      top_padding = 18;
+      bottom_padding = 46;
+      left_padding = 18;
+      right_padding = 18;
+      # shadows and borders
+      focus_follows_mouse = "off";
+      mouse_follows_focus = "on";
+      window_shadow = "on";
+      window_border = "off";
+      window_border_width = 3;
       window_opacity = "on";
-      window_topmost = "on";
-      window_shadow = "float";
       active_window_opacity = "1.0";
       normal_window_opacity = "1.0";
-      split_ratio = "0.50";
-      auto_balance = "off";
-      mouse_modifier = "fn";
+      # mouse
+
+      # mouse_modifier = "cmd";
       mouse_action1 = "move";
       mouse_action2 = "resize";
-      layout = "bsp";
-      top_padding = 0;
-      bottom_padding = 0;
-      left_padding = 0;
-      right_padding = 0;
-      window_gap = 0;
-      external_bar = "main:0:0";
+      mouse_drop_action = "swap";
     };
-
-    extraConfig = pkgs.lib.mkDefault ''
+    extraConfig = ''
       # rules
+      # yabai -m rule --add app=emacs-29.0.50 manage=on
+      # yabai -m rule --add app='Firefox Nightly' manage=on
       yabai -m rule --add app='System Preferences' manage=off
-      yabai -m rule --add app='Live' manage=off
-      yabai -m rule --add app='Xcode' manage=off
+      # yabai -m rule --add app='Activity Monitor' manage=off
     '';
+  };
+
+  services.spacebar = {
+    enable = true;
+    package = pkgs.spacebar;
+    config = {
+      position = "bottom";
+      height = 28;
+      title = "on";
+      spaces = "on";
+      power = "on";
+      clock = "off";
+      right_shell = "off";
+      padding_left = 20;
+      padding_right = 20;
+      spacing_left = 25;
+      spacing_right = 25;
+      text_font = ''"Menlo:16.0"'';
+      icon_font = ''"Menlo:16.0"'';
+      background_color = "0xff161616";
+      foreground_color = "0xffFFFFFF";
+      space_icon_color = "0xff3ddbd9";
+      power_icon_strip = " ";
+      space_icon_strip = "一 二 三 四 五 六 七 八 九 十";
+      spaces_for_all_displays = "on";
+      display_separator = "on";
+      display_separator_icon = "|";
+      clock_format = ''"%d/%m/%y %R"'';
+      right_shell_icon = " ";
+      right_shell_command = "whoami";
+    };
   };
 
   launchd.user.agents.yabai.serviceConfig.StandardErrorPath = "/tmp/yabai.log";
@@ -355,10 +235,10 @@
     ];
 
     masApps = {
-      Amphetamine = 937984704;
-      Pages = 409201541;
-      Keynote = 409183694;
-      "Surfshark: Unlimited VPN Proxy" = 1437809329;
+      # Amphetamine = 937984704;
+      # Pages = 409201541;
+      # Keynote = 409183694;
+      # "Surfshark: Unlimited VPN Proxy" = 1437809329;
     };
   };
 
