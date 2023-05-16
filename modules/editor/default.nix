9727de4102bf78e89a73eac7ccb5165ad1843352
Josep Lluis Giralt D'Lacoste <josepgiraltdlacoste@gmail.com>
2023-05-16 13:24:30 +0100

    Restore prettier

--- modules/editor/default.nix
+++ modules/editor/default.nix
@@ -165,6 +165,23 @@
       \   }
       \ }
 
+      let g:neoformat_prettier = {
+        \ 'exe': '/etc/profiles/per-user/pepo/bin/prettier',
+        \ 'args': ['--stdin-filepath', '"%:p"'],
+        \ 'stdin': 1,
+        \ }
+ 
+      let g:neoformat_javascript_prettier = g:neoformat_prettier
+      let g:neoformat_enabled_javascript = ['prettier']
+      let g:neoformat_json_prettier = g:neoformat_prettier
+      let g:neoformat_enabled_json = ['prettier']
+      let g:neoformat_css_prettier = g:neoformat_prettier
+      let g:neoformat_enabled_css = ['prettier']
+      let g:neoformat_typescript_prettier = g:neoformat_prettier
+      let g:neoformat_enabled_typescript = ['prettier']
+      let g:neoformat_yaml_prettier = g:neoformat_prettier
+      let g:neoformat_enabled_yaml = ['prettier']
+         
       autocmd BufWritePre *.ex,*.exs,*.eex,*.leex,*.heex lua vim.lsp.buf.format()
 
       lua << EOF
