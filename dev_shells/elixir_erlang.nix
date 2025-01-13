4dfd4cb3acd744cb1fdca3c0ec0db9bde94cf00a
Josep Lluis Giralt D'Lacoste <josepgiraltdlacoste@gmail.com>
2025-01-13 21:24:00 +0000

    Refactor dev_shells/default.nix and elixir_erlang.nix: Add new Elixir/Erlang shell configurations.

--- dev_shells/elixir_erlang.nix
+++ dev_shells/elixir_erlang.nix
@@ -4,20 +4,24 @@ inputs:
 let
   overlays = [
     (self: super: rec {
-      erlang = super.beam.interpreters.erlangR25.override {
-        version = inputs.erlangVersion;
+      erlang = super.beam.interpreters.${inputs.erlangInterpreter}.override {
         sha256 = inputs.erlangSha256;
+        version = inputs.erlangVersion;
       };
 
-      elixir = (super.beam.packagesWith erlang).elixir.override {
-        version = inputs.elixirVersion;
+      elixir = (super.beam.packagesWith erlang).elixir.override ({
         sha256 = inputs.elixirSha256;
-      };
+        version = inputs.elixirVersion;
+      } // (if inputs ? elixirEscriptPath then {
+        escriptPath = inputs.elixirEscriptPath;
+      } else
+        { }));
     })
   ];
   system = inputs.system;
   pkgs = import inputs.nixpkgs { inherit system overlays; };
-in pkgs.mkShell {
+in
+pkgs.mkShell {
   buildInputs = with pkgs;
     let
       linuxPackages =
@@ -28,22 +32,25 @@ in pkgs.mkShell {
           CoreFoundation
           CoreServices
         ]);
-    in builtins.concatLists [ [ erlang elixir ] linuxPackages darwinPackages ];
+    in
+    builtins.concatLists [ [ erlang elixir ] linuxPackages darwinPackages ];
 
-  shellHook = let
-    escript = ''
-      Filepath = filename:join([
-        code:root_dir(),
-        "releases",
-        erlang:system_info(otp_release),
-        "OTP_VERSION"
-      ]),
-      {ok, Version} = file:read_file(Filepath),
-      io:fwrite(Version),
-      halt().
+  shellHook =
+    let
+      escript = ''
+        Filepath = filename:join([
+          code:root_dir(),
+          "releases",
+          erlang:system_info(otp_release),
+          "OTP_VERSION"
+        ]),
+        {ok, Version} = file:read_file(Filepath),
+        io:fwrite(Version),
+        halt().
+      '';
+    in
+    ''
+      echo "🍎 Erlang OTP-$(erl -eval '${escript}' -noshell)"
+      echo "💧 $(${pkgs.elixir}/bin/elixir --version | tail -n 1)"
     '';
-  in ''
-    echo "🍎 Erlang OTP-$(erl -eval '${escript}' -noshell)"
-    echo "💧 $(${pkgs.elixir}/bin/elixir --version | tail -n 1)"
-  '';
 }
